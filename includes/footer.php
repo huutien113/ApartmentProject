ca64819418d5e98944e0e597bd4c99e6fd86fbbc
Thiendaocuti <kellydao002@gmail.com>
2026-03-10 14:50:23 +0700

    h

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,5 @@
-</main>
 <footer>
-    <p>&copy; <?php echo date('Y'); ?> Quản Lý Chung Cư. All rights reserved.</p>
-</footer>
-<script src="/asset/js/script.js"></script>
+        <p>Copyright &copy; 2026 ChungCu.net. Thiết kế bởi Nhóm Đồ Án.</p>
+    </footer>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quản Lý Chung Cư</title>
-    <link rel="stylesheet" href="/asset/css/style.css">
+    <title>Sàn Giao Dịch Chung Cư</title>
+    <link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body>
-<header>
-    <nav>
-        <div class="logo">Quản Lý Chung Cư</div>
-        <ul>
-            <li><a href="/index.php">Trang Chủ</a></li>
-            <li><a href="/admin/index.php">Quản Trị</a></li>
-        </ul>
-    </nav>
-</header>
-<main>
+    <header>
+        <div class="logo">ChungCu<span style="color: #2c3e50;">.net</span></div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Trang Chủ</a></li>
+                <li><a href="#">Dự Án Đang Mở Bán</a></li>
+                <li><a href="#">Tin Tức</a></li>
+                <li><a href="#">Ký Gửi</a></li>
+            </ul>
+        </nav>
+    </header>
